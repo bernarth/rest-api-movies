e35a5a073dde5e2f4b5d4314cac783372dac7925
Bernarth <alberthbernarth75@gmail.com>
2025-07-13 22:46:04 -0400

    add database to project with dapper and npgsql

--- Movies.Api/Program.cs
+++ Movies.Api/Program.cs
@@ -1,10 +1,14 @@
 using Movies.Application;
+using Movies.Application.Database;
 
 var builder = WebApplication.CreateBuilder(args);
+var config = builder.Configuration;
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
+
 builder.Services.AddApplication();
+builder.Services.AddDatabase(config["Database:ConnectionString"]!);
 
 var app = builder.Build();
 
@@ -17,4 +21,8 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 app.UseAuthorization();
 app.MapControllers();
+
+var dbInitializer = app.Services.GetRequiredService<DbInitializer>();
+await dbInitializer.InitializeAsync();
+
 app.Run();
---
+++ Movies.Application/Database/DbConnectionFactory.cs
@@ -0,0 +1,22 @@
+﻿using Npgsql;
+using System.Data;
+
+namespace Movies.Application.Database;
+
+public interface IDbConnectionFactory
+{
+    Task<IDbConnection> CreateConnectionAsync();
+}
+
+public class NpgsqlConnectionFactory(string connectionString) : IDbConnectionFactory
+{
+    private readonly string _connectionString = connectionString;
+
+    public async Task<IDbConnection> CreateConnectionAsync()
+    {
+        var connection = new NpgsqlConnection(_connectionString);
+        await connection.OpenAsync();
+
+        return connection;
+    }
+}
