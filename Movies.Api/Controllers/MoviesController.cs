870e455789d8b956f5cd1e63d9eb384daeddb5ad
Bernarth <alberthbernarth75@gmail.com>
2025-09-01 23:45:06 -0400

    add filtering system (not generic)

--- Movies.Api/Controllers/MoviesController.cs
+++ Movies.Api/Controllers/MoviesController.cs
@@ -45,10 +45,12 @@ public class MoviesController(IMovieService movieService) : ControllerBase
     }
 
     [HttpGet(ApiEndpoints.Movies.GetAll)]
-    public async Task<IActionResult> GetAll(CancellationToken token)
+    public async Task<IActionResult> GetAll([FromQuery] GetAllMoviesRequest request, CancellationToken token)
     {
         Guid? userId = HttpContext.GetUserId();
-        IEnumerable<Movie> movies = await _movieService.GetAllAsync(userId, token);
+        GetAllMoviesOptions options = request.MapToOptions()
+            .WithUser(userId);
+        IEnumerable<Movie> movies = await _movieService.GetAllAsync(options, token);
         MoviesReponse response = movies.MapToResponse();
         
         return Ok(response);
--- Movies.Application/Repositories/MovieRepository.cs
+++ Movies.Application/Repositories/MovieRepository.cs
@@ -97,7 +97,7 @@ public class MovieRepository(IDbConnectionFactory dbConnectionFactory) : IMovieR
         return movie;
     }
 
-    public async Task<IEnumerable<Movie>> GetAllAsync(Guid? userId = default, CancellationToken token = default)
+    public async Task<IEnumerable<Movie>> GetAllAsync(GetAllMoviesOptions options, CancellationToken token = default)
     {
         using var connection = await _dbConnectionFactory.CreateConnectionAsync(token);
         var result = await connection.QueryAsync(new CommandDefinition("""
@@ -110,8 +110,15 @@ public class MovieRepository(IDbConnectionFactory dbConnectionFactory) : IMovieR
             LEFT JOIN ratings r ON m.id = r.movieid
             LEFT JOIN ratings myr ON m.id = myr.movieid
                 AND myr.userid = @userId
+            WHERE (@title IS NULL OR m.title like(% || @title || %))
+            AND (@yearofrelease IS NULL OR m.yearofrelease = @yearofrelease)
             GROUP BY id, userrating
-            """, new { userId }, cancellationToken: token));
+            """, new
+        { 
+            userId = options.UserId,
+            title = options.Title,
+            yearofrelease = options.YearOfRelease
+        }, cancellationToken: token));
 
         return result.Select(x => new Movie
         {
